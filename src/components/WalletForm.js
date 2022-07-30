95f18209b2c9ccc7cb9e3dd8d33d9994c9da527a
Isabelly Rocha Vieira <isabellyrvieira@icloud.com>
2022-07-29 21:04:31 -0300

    req-03

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -1,9 +1,75 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { getApiAction } from '../redux/actions';
 
-export default class WalletForm extends Component {
+class WalletForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
+
+  componentDidMount() {
+    const { valorMoeda } = this.props;
+    valorMoeda();
+  }
+
   render() {
+    const { currencies } = this.props;
     return (
-      <div>WalletForm</div>
+      <div>
+        WalletForm
+        <form>
+          <label htmlFor="value">
+            Valor
+            <input
+              id="value"
+              name="value"
+              data-testid="value-input"
+              type="text"
+            />
+          </label>
+          <label htmlFor="description">
+            <input
+              id="description"
+              name="description"
+              data-testid="description-input"
+              type="text"
+            />
+          </label>
+          <select data-testid="currency-input">
+            {currencies.map((curr) => <option key={ curr }>{curr}</option>)}
+          </select>
+
+          <select data-testid="method-input">
+            <option>Dinheiro</option>
+            <option>Cartão de crédito</option>
+            <option>Cartão de débito</option>
+          </select>
+
+          <select data-testid="tag-input">
+            <option>Alimentação</option>
+            <option>Lazer</option>
+            <option>Trabalho</option>
+            <option>Transporte</option>
+            <option>Saúde</option>
+
+          </select>
+        </form>
+      </div>
     );
   }
 }
+const mapDispatchToProps = (dispatch) => ({
+  valorMoeda: (payload) => dispatch(getApiAction(payload)) });
+
+const mapStateToProps = (state) => ({
+  currencies: state.wallet.currencies,
+});
+
+WalletForm.propTypes = {
+  valorMoeda: PropTypes.func.isRequired,
+  currencies: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+export default connect(mapStateToProps, mapDispatchToProps)(WalletForm);
