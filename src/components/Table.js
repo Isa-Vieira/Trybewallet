a73a6c9868f0bec155e30bf8e3286986dc2e9459
Isabelly Rocha Vieira <isabellyrvieira@icloud.com>
2022-08-02 15:29:55 -0300

    req-07-concluido

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-export default class Table extends Component {
+class Table extends Component {
   render() {
+    const { expensesTable } = this.props;
     return (
       <div>
         Table
@@ -19,8 +22,38 @@ export default class Table extends Component {
               <th>Editar/Excluir</th>
             </tr>
           </thead>
+          <tbody>
+            {expensesTable.map((param) => (
+              <tr key={ param.id }>
+                <td>{ param.description}</td>
+                <td>{ param.tag }</td>
+                <td>{ param.method }</td>
+                <td>{(Number(param.value)).toFixed(2) }</td>
+                <td>{ param.currency }</td>
+                <td>{(Number(param.exchangeRates[param.currency].ask)).toFixed(2) }</td>
+                <td>
+                  {(Number(param.exchangeRates[param.currency]
+                    .ask * (Number(param.value)))).toFixed(2)}
+                </td>
+                <td>{param.exchangeRates[param.currency].name}</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     );
   }
 }
+const mapStateToProps = (state) => ({
+  expensesTable: state.wallet.expenses,
+});
+
+/* const mapDispatchToProps = (dispatch) => {
+
+}; */
+
+Table.propTypes = {
+  expensesTable: PropTypes.array.isRequired,
+}.isRequired;
+
+export default connect(mapStateToProps)(Table);
